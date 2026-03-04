caa21b052818e6c9b4ab4a089cb53d496e22b44a
rileyracer62@yahoo.com <rileyracer62@yahoo.com>
2026-03-03 16:04:21 -0800

    updated user model in accounts

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+  """
+  Custom user model for QuickSave.
+  Extends Django's build in AbstractUser so we keep all the
+  default auth functionality (login, password hashing, etc.)
+  while giving ourselves room to add fields later. 
+  """
+
+  # Future fields can go here, eg:
+  # avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+  # bio = models.TextField(blank=True)
+  # timezone = models.CharField(max_length=50, default='UTC')
+
+  def __str__(self):
+    return self.username
+  
