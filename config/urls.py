701c2f276507d1f6a83d323358b3e5a3d16c830e
rileyracer62@yahoo.com <rileyracer62@yahoo.com>
2026-03-06 16:14:32 -0800

    fixed config url import error on accounts

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.http import FileResponse
+from accounts import views as account_views
 import os
 
 
